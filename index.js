3f2515e2a8ffd21022cdaa16efe3a70108b60569
Devon Darrow <ddarrowphl@gmail.com>
2018-07-23 13:23:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ function yourAnimal() {
 
 function add2(n) {
   //const two = 2
-  const two = n + 2 
-  return n + two
+  const two = n + 2
+  return two
   //return n + two
 
   // Feel free to move things around!
